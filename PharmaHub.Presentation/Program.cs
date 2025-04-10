c3ac6ab61484ae2b6e729709cf307f7e97a40d68
RouQandel <rawanqandel62@gmail.com>
2025-04-11 00:07:08 +0200

    Enhance Program.cs with repository integration
    Updated `Program.cs` to include additional using directives for `GenericRepository`, `SuggestedMedicine`, and `UnitOfWork`. Restructured the `Main` method for improved readability and organization, adding comments for clarity. Adjusted the order of service registration and ensured Swagger is configured for development mode. Removed unnecessary lines to streamline the code and enhance maintainability.

--- PharmaHub.Presentation/Program.cs
+++ PharmaHub.Presentation/Program.cs
@@ -1,36 +1,50 @@
 
-namespace PharmaHub.Presentation
+using PharmaHub.DAL.Repositories.GenericRepository;
+using PharmaHub.DAL.Repositories.GenericRepositoryl;
+using PharmaHub.DAL.Repositories.SuggestedMedicin;
+using PharmaHub.DAL.Repositories.UnitOfWork;
+using PharmaHub.DALRepositories;
+
+namespace PharmaHub.Presentation;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+
+        // Register Generic Repository
+        builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+
+        // Register Specific Repositories
+        builder.Services.AddScoped<ISuggestedMedicineRepository, SuggestedMedicineRepository>();
+
+        // Register Unit of Work
+        builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+
+        app.MapControllers();
+
+        app.Run();
     }
 }
