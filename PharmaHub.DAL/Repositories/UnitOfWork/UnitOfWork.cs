6097bcc546c3bf119e46f5e2adcdd36c05c17c9a
RouQandel <rawanqandel62@gmail.com>
2025-04-10 23:28:03 +0200

    Add UnitOfWork class for repository management
    Introduces a new `UnitOfWork` class in the `PharmaHub.DAL.Repositories.UnitOfWork` namespace. This class manages the `ApplicationDbContext` and implements the repository pattern, including a caching mechanism for repositories. A specific repository for suggested medicines is provided, along with an asynchronous `CompleteAsync` method for saving changes to the database.

---
+++ PharmaHub.DAL/Repositories/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,41 @@
+﻿using PharmaHub.DAL.Context;
+using PharmaHub.DAL.Repositories.GenericRepository;
+using PharmaHub.DAL.Repositories.GenericRepositoryl;
+using PharmaHub.DAL.Repositories.SuggestedMedicin;
+using PharmaHub.DALRepositories;
+
+namespace PharmaHub.DAL.Repositories.UnitOfWork;
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly ApplicationDbContext _context;
+
+    private Dictionary<Type, object> _repositories =  new();
+
+    public ISuggestedMedicineRepository SuggestedMedicineRepository => new SuggestedMedicineRepository(_context);
+    // other repositories...
+
+    public UnitOfWork(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public IGenericRepository<T> Repository<T>() where T : class
+    {
+        var type = typeof(T);
+        if (_repositories.TryGetValue(type, out var instance))
+        {
+            return (IGenericRepository<T>)instance;
+        }
+
+        var repositoryType = typeof(GenericRepository<>);
+        var repositoryInstance = Activator.CreateInstance(
+            repositoryType.MakeGenericType(type), _context) as IGenericRepository<T>;
+
+        _repositories.TryAdd(type, repositoryInstance);
+        return repositoryInstance;
+    }
+
+    public async Task<int> CompleteAsync()
+    => await _context.SaveChangesAsync();
+
+}
