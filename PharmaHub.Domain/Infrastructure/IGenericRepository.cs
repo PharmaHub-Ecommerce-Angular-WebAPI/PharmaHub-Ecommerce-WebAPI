d2d4f4aab44821cda2d8b149735602eaf59fdf6f
RouQandel <rawanqandel62@gmail.com>
2025-04-10 23:28:54 +0200

    Add repository interfaces for better data access
    Introduce `IGenericRepository<T>` for CRUD operations and
    `ISuggestedMedicineRepository` for user-specific medicine
    management. Update `IUnitOfWork` to include the new
    repository and enhance transaction handling. These changes
    improve the organization and reusability of data access logic
    in the PharmaHub project.

---
+++ PharmaHub.Domain/Infrastructure/IGenericRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace PharmaHub.DAL.Repositories.GenericRepository;
+
+public interface IGenericRepository <T> where T : class
+{
+    Task<T> AddAsync(T entity);
+    Task<bool> DeleteAsync(Guid id);
+    Task<T?> GetIdAsync(Guid id);
+    Task<IReadOnlyList<T>> GetAllAsync();
+    Task<T> UpdatedAsync(T entity);
+    Task UpsertAsync(T entity);
+    Task SavaChange();
+}
---
+++ PharmaHub.Domain/Infrastructure/ISuggestedMedicineRepository.cs
@@ -0,0 +1,10 @@
+﻿using PharmaHub.DAL.Repositories.GenericRepository;
+using PharmaHub.Domain.Entities;
+
+namespace PharmaHub.DAL.Repositories.SuggestedMedicin;
+
+public interface ISuggestedMedicineRepository 
+{
+    Task<IEnumerable<SuggestedMedicine>> GetAllByUserAsync(Guid userId);
+    Task AddSuggestedMedicineAsync(SuggestedMedicine newMed);
+}
