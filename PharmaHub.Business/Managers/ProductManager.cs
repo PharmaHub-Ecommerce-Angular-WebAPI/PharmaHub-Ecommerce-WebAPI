ddf0d06307b80d368166c3999cee8ed473256759
Mohamedmahdy404 <mohamedmahdy3162@gmail.com>
2025-04-15 19:15:54 +0200

    update project references
    - Removed references to PharmaHub.DAL in Business project.
    - Updated DTOs project to reference PharmaHub.DAL instead of Domain and Service.

--- PharmaHub.Business/Managers/ProductManager.cs
+++ PharmaHub.Business/Managers/ProductManager.cs
@@ -3,10 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PharmaHub.DAL.Repositories;
+using PharmaHub.Domain.Entities;
 
 namespace PharmaHub.Business.Managers
 {
     public class ProductManager
     {
+        private IUnitOfWork _unitOfWork;
+        public ProductManager(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        #region Customer
+
+        #region HomePage
+
+        public async Task<IReadOnlyList<Product>> GetLatestProductsAsync(int page, int size)
+        {
+            
+        }
+
+        #endregion
+        
+
+        #endregion
+
+
+
     }
 }
