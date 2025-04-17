74c143fc0c7f71117e2ff66d4b7040d115607e1c
Mohamedmahdy404 <mohamedmahdy3162@gmail.com>
2025-04-17 05:39:22 +0200

    Add controllers for favorites, products, and medicines
    This commit introduces the `FavoriteProductsController`, `ProductsController`, and `SuggestedMedicinesController` to manage API endpoints for favorite products, products, and suggested medicines. The `ISuggestedMedicineRepository` interface is updated to include a new method for adding suggested medicines. Each controller is equipped with methods for adding, removing, and retrieving items, enhancing the application's overall functionality.

---
+++ PharmaHub.Presentation/Controllers/SuggestedMedicinesController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PharmaHub.Business.Contracts;
+using PharmaHub.DTOs.SuggestedMedicineDTOs;
+
+namespace PharmaHub.Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuggestedMedicinesController : ControllerBase
+    {
+        private readonly ISuggestedMedicineManager _suggestedManager;
+
+        public SuggestedMedicinesController(ISuggestedMedicineManager suggestedManager)
+        {
+            _suggestedManager = suggestedManager;
+        }
+
+        // POST: api/suggestedmedicines
+        [HttpPost]
+        public async Task<IActionResult> AddSuggestedMedicine([FromBody] CreateSuggestedMedicineDto newMed)
+        {
+            await _suggestedManager.AddSuggestedMedicineAsync(newMed);
+            return StatusCode(201); // Created
+        }
+
+        // DELETE: api/suggestedmedicines/{id}
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSuggestedMedicine(Guid id)
+        {
+            await _suggestedManager.RemoveSuggestedMedicineAsync(id);
+            return NoContent();
+        }
+
+        // GET: api/suggestedmedicines/search?name=paracetamol
+        [HttpGet("search")]
+        public async Task<IActionResult> SearchSuggestedMedicine([FromQuery] string name)
+        {
+            var results = await _suggestedManager.SearchSuggestedMedicineAsync(name);
+            if (results == null || !results.Any())
+            {
+                return NotFound("Medicine Not Found");
+            }
+            return Ok(results);
+        }
+
+
+
+    }
+}
